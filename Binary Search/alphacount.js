4d6468efe5de22f6330cbf4e68f5ced4b6c2c42e
Gyanendra prakash <pgyanendra333@gmail.com>
2023-01-15 22:19:26 +0530

    Alphabet Number Count

---
+++ Binary Search/alphacount.js
@@ -0,0 +1,15 @@
+
+
+let word = "abcdefagha";
+let map = new Map([]);
+
+for (let i = 0; i < word.length; i++) {
+    if (map.has(word[i])) {
+        let val = map.get(word[i])
+        map.set(word[i], val + 1)
+    }
+    else {
+        map.set(word[i], 1)
+    }
+}
+console.log(map);
